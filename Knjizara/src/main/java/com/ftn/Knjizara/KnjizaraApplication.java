a77ba1b2a97f33e893709e51210a9aef54c99abb
jokanovicc <vladimirjokanovicc@gmail.com>
2021-01-02 17:41:08 +0100

    Inicijalizacija projekta

---
+++ Knjizara/src/main/java/com/ftn/Knjizara/KnjizaraApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.Knjizara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnjizaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnjizaraApplication.class, args);
+	}
+
+}
---
+++ Knjizara/src/test/java/com/ftn/Knjizara/KnjizaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.Knjizara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnjizaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
